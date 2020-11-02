3be60546886d67a3acac20d68a325a8dd2d7482a
judas1995 <judas123684@gmail.com>
2020-11-02 14:54:37 +0800

    刪掉#字測試行

--- letmebash_newdata.sh
+++ letmebash_newdata.sh
@@ -21,7 +21,6 @@ tmp_file04=tmp_file04_tmp.txt
 tmp_file05=tmp_file05_tmp.txt
 
 
-#hg[kosfkdojdoewjdiosA:awdawdawddawdawdawF]
 #定義資料位置(bp是建立在我拿的資料如果已經band pass過後)
 ls $data_path/*bp >$tmp_path/$tmp_file04
 
